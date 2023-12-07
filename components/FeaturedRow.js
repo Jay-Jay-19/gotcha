bb38165705d4e83e6d37795db65275dd646b8822
gilbert mougamadou <gilbertmougamadou@yahoo.fr>
2023-12-07 17:36:30 +0100

    edit HomeScreen.js, FeaturedRow.js and RestaurantCard.js. Featured categories in the homepage is now fully connected to the backend

--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -1,9 +1,35 @@
 import { View, Text, ScrollView } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ArrowRightIcon } from 'react-native-heroicons/outline';
 import RestaurantCard from './RestaurantCard';
+import client from '../sanity';
 
 const FeaturedRow = ({ id, title, description }) => {
+
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    client.fetch(
+      `
+        *[_type == "featured" && _id == $id] {
+          ...,
+            restaurants[]->{
+              ...,
+                dishes[]->,
+                  type->{
+                    name
+                  }
+            },
+        }[0]
+      `,
+      { id }
+    ).then(data => {
+      setRestaurants(data?.restaurants);
+    });
+  }, []);
+
+  console.log(restaurants);
+
   return (
     <View>
 
@@ -22,46 +48,22 @@ const FeaturedRow = ({ id, title, description }) => {
         showsHorizontalScrollIndicator={false}
         className="pt-4"
       >
-        {/* RestaurantCards... */}
-        <RestaurantCard
-          id={123}
-          imgUrl="https://asiankitchen.fr/wp-content/uploads/2022/11/sushi-768x512.jpg"
-          title="Hokkaido Sushi"
-          rating={4.5}
-          genre="Japonais"
-          address="45 rue du Poteau"
-          short_description="This is a test description"
-          dishes={[]}
-          long={20}
-          lat={0}
-        />
-        <RestaurantCard
-          id={123}
-          imgUrl="https://asiankitchen.fr/wp-content/uploads/2022/11/sushi-768x512.jpg"
-          title="Hokkaido Sushi"
-          rating={4.5}
-          genre="Japonais"
-          address="45 rue du Poteau"
-          short_description="This is a test description"
-          dishes={[]}
-          long={20}
-          lat={0}
-        />
-        <RestaurantCard
-          id={123}
-          imgUrl="https://asiankitchen.fr/wp-content/uploads/2022/11/sushi-768x512.jpg"
-          title="Hokkaido Sushi"
-          rating={4.5}
-          genre="Japonais"
-          address="45 rue du Poteau"
-          short_description="This is a test description"
-          dishes={[]}
-          long={20}
-          lat={0}
-        />
-
+        {restaurants?.map(restaurant => (
+          <RestaurantCard
+            key={restaurant._id}
+            id={restaurant._id}
+            imgUrl={restaurant.image}
+            title={restaurant.name}
+            rating={restaurant.rating}
+            genre={restaurant.type?.name}
+            address={restaurant.address}
+            short_description={restaurant.short_description}
+            dishes={restaurant.dishes}
+            long={restaurant.long}
+            lat={restaurant.lat}
+          />
+        ))}
       </ScrollView>
-
     </View>
   );
 };
