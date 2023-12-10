8c4835c19316d49b0a7714444a198e4faba79b36
gilbert mougamadou <gilbertmougamadou@yahoo.fr>
2023-12-10 20:00:35 +0100

    create DishRow.js

--- App.js
+++ App.js
@@ -19,4 +19,4 @@ export default function App() {
       </TailwindProvider>
     </NavigationContainer>
   );
-}
+};
--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,9 +1,10 @@
-import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
-import React, { useLayoutEffect } from 'react'
-import { useNavigation, useRoute } from '@react-navigation/native'
+import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native';
+import React, { useLayoutEffect } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { urlFor } from '../sanity';
 import { ArrowLeftIcon, ChevronRightIcon, StarIcon } from 'react-native-heroicons/solid';
 import { MapPinIcon, QuestionMarkCircleIcon } from 'react-native-heroicons/outline';
+import DishRow from '../components/DishRow';
 
 const RestaurantScreen = () => {
 
@@ -62,15 +63,40 @@ const RestaurantScreen = () => {
 
             <View className="flex-row items-center space-x-1">
               <MapPinIcon color="gray" opacity={0.4} size={22} />
-              <Text className="text-xs text-gray-500">
-                <Text className="text-xs text-gray-500">Nearby </Text>
-              </Text>
+                <Text className="text-xs text-gray-500">Nearby  •  {address}</Text>
             </View>
           </View>
+
+          <Text className="text-gray-500 mt-2 pb-4" >{short_description}</Text>
         </View>
+
+        <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
+          <QuestionMarkCircleIcon color="gray" opacity={0.6} size={20} />
+          <Text className="pl-2 flex-1 text-md font-bold">
+            Allergènes
+          </Text>
+          <ChevronRightIcon color="#004aad" />
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
+
+        {/* Dishrows */}
+        
+        {dishes.map((dish) => (
+          <DishRow
+            key={dish._id}
+            dish={dish._id}
+            name={dish.name}
+            description={dish.short_description}
+            price={dish.price}
+            image={dish.image}
+          />
+        ))}
       </View>
     </ScrollView>
   )
 }
 
-export default RestaurantScreen
+export default RestaurantScreen;
