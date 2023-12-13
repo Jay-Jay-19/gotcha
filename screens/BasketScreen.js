69aad179b06f36ac805b798b9d977dcb3aca1f49
gilbert mougamadou <gilbertmougamadou@yahoo.fr>
2023-12-13 15:17:18 +0100

    edit BasketScreen

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,9 +1,13 @@
-import { View, Text, SafeAreaView } from 'react-native';;
+import { View, Text, TouchableOpacity, Image, ScrollView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import React, { useMemo, useState } from 'react'
 import { useNavigation } from '@react-navigation/native';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectRestaurant } from '../features/restaurantSlice';
-import { selectBasketItems } from '../features/basketSlice';
+import { selectBasketItems, selectBasketTotal } from '../features/basketSlice';
+import { XCircleIcon } from 'react-native-heroicons/solid';
+import { urlFor } from '../sanity';
+import { removeFromBasket } from '../features/basketSlice';
 
 const BasketScreen = () => {
   
@@ -12,6 +16,9 @@ const BasketScreen = () => {
   const items = useSelector(selectBasketItems);
   const [groupedItemsInBasket, setGroupedItemsInBasket] = useState([]);
   const dispatch = useDispatch();
+  const basketTotal = useSelector(selectBasketTotal);
+  const deliveryFee = 5.99;
+  const serviceFee = 2.99;
 
   useMemo(() => {
     const groupedItems = items.reduce((results, item) => {
@@ -22,20 +29,100 @@ const BasketScreen = () => {
     setGroupedItemsInBasket(groupedItems);
   }, [items]);
 
-  console.log(groupedItemsInBasket);
-
   return (
-    <SafeAreaView>
-      <View>
-        <View>
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="flex-1 bg-gray-100">
+        <View className="p-5 border-b border-[#004aad] bg-white shadow-xs">
           <View>
             <Text className="text-lg font-bold text-center">Panier</Text>
             <Text className="text-center text-gray-400">{restaurant.title}</Text>
           </View>
+
+          <TouchableOpacity
+          onPress={navigation.goBack}
+          className="rounded-full absolute top-3 right-5"
+          >
+            <XCircleIcon color="#004aad" height={35} width={35}/>
+          </TouchableOpacity>
+        </View>
+
+        <View className="flex-row items-center space-x-4 px-4 py-3 bg-white my-5">
+          <Image
+            source={require("./gotcha_logo.png")}
+            className='h-10 w-10 p-4 rounded-full'
+          />
+          <Text className="flex-1">Livré dans 30 à 45 min</Text>
+          <TouchableOpacity>
+            <Text className="text-[#004aad]">Changer</Text>
+          </TouchableOpacity>
+        </View>
+
+        <ScrollView className="divide-y divide-gray-200">
+          {Object.entries(groupedItemsInBasket).map(([key, items]) => (
+            <View
+              key={key}
+              className="flex-row items-center space-x-3 bg-white py-2 px-5"
+            >
+              <Text className="text-[#004aad]">{items.length} x </Text>
+              <Image
+                source={{ uri: urlFor(items[0]?.image).url() }}
+                className="h-12 w-12 rounded-full"   
+              />
+              <Text className="flex-1">{items[0]?.name}</Text>
+
+              <Text className="text-gray-600">
+                {Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(items[0]?.price)}
+              </Text>
+
+              <TouchableOpacity>
+                <Text
+                  className="text-[#004aad] text-xs"
+                  onPress={() => dispatch(removeFromBasket({ id: key }))}
+                >
+                  Supprimer
+                </Text>
+              </TouchableOpacity>
+            </View>
+          ))}
+        </ScrollView>
+
+        <View className="p-5 bg-white mt-5 space-y-4">
+          <View className="flex-row justify-between">
+            <Text className="text-gray-400">Sous-total</Text>
+            <Text className="text-gray-400">
+              {Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(basketTotal)}
+            </Text>
+          </View>
+
+          <View className="flex-row justify-between">
+            <Text className="text-gray-400">Frais de livraison</Text>
+            <Text className="text-gray-400">
+              {Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(deliveryFee)}
+            </Text>
+          </View>
+
+          <View className="flex-row justify-between">
+            <Text className="text-gray-400">Frais de service</Text>
+            <Text className="text-gray-400">
+              {Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(serviceFee)}
+            </Text>
+          </View>
+
+          <View className="flex-row justify-between">
+            <Text className="font-extrabold">Total de la commande</Text>
+            <Text className="font-extrabold">
+              {Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(basketTotal+deliveryFee+serviceFee)}
+            </Text>
+          </View>
+
+          <TouchableOpacity className="rounded-lg bg-[#ff3131] p-4">
+            <Text className="text-center text-white text-lg font-bold">Commander</Text>
+          </TouchableOpacity>
+
         </View>
       </View>
     </SafeAreaView>
   )
-}
+};
 
 export default BasketScreen
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -47,7 +47,7 @@ const HomeScreen = () => {
       <View className="flex-row pb-3 items-center mx-4 space-x-2">
         <Image
           source={require("./gotcha_logo.png")}
-          className='h-10 w-10 bg-gray-300 p-4 rounded-full'
+          className='h-10 w-10 p-4 rounded-full'
         />
         <View className="flex-1">
           <Text className="font-bold text-gray-400 text-xs">Livraison immédiate</Text>
