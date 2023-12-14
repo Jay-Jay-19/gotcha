5fbf1f14f47d5c466e8e741bb12726b16d1fd2fb
gilbert mougamadou <gilbertmougamadou@yahoo.fr>
2023-12-14 16:52:12 +0100

    edit PreparingOrderScreen.js

--- screens/PreparingOrderScreen.js
+++ screens/PreparingOrderScreen.js
@@ -2,12 +2,21 @@ import { View, Text } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import * as Animatable from 'react-native-animatable';
-import * as Progress from 'react-native-progress;'
+import LottieView from 'lottie-react-native';
+import { useNavigation } from '@react-navigation/native';
+// import * as Progress from 'react-native-progress';
 
 const PreparingOrderScreen = () => {
 
+  const navigation = useNavigation();
+
+    
+
+  const loaderAnimation = require("../assets/loaderAnimation.json");
+
   return (
     <SafeAreaView className="bg-white flex-1 justify-center items-center">
+      
       <Animatable.Image
         source={require("../assets/orderLoading.gif")}
         animation="slideInUp"
@@ -18,12 +27,19 @@ const PreparingOrderScreen = () => {
       <Animatable.Text
         animation="slideInUp"
         iterationCount={1}
-        className="text-lg my-10 font-bold text-center"
+        className="text-lg text-[#004aad] my-10 font-bold text-center"
       >
         En attente de la confirmation du restaurant !
       </Animatable.Text>
 
-      <Progress.Circle size={60} indeterminate={true} color="#004aad"/>
+      <LottieView
+        source={loaderAnimation}
+        autoPlay
+        loop
+        style={{ width: '40%' }}
+      />
+
+      {/* <Progress.Circle size={60} indeterminate={true} color="#004aad" borderWidth={3} indeterminateAnimationDuration={1500}/> */}
     </SafeAreaView>
   );
 };
