6c666259cc4b5445c024f31561fac4e284725c35
gilbert mougamadou <gilbertmougamadou@yahoo.fr>
2023-12-15 22:41:13 +0100

    edit DeliveryScreen.js - Incorporated google maps though a degraded map. Second SafeAreaView is too large

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -1,10 +1,12 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { View, Text, TouchableOpacity, Image, } from 'react-native';
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { useSelector } from 'react-redux';
 import { selectRestaurant } from '../features/restaurantSlice';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { XMarkIcon } from 'react-native-heroicons/solid';
+import * as Progress from 'react-native-progress';
+import MapView, { Marker } from 'react-native-maps';
 
 const DeliveryScreen = () => {
 
@@ -12,28 +14,68 @@ const DeliveryScreen = () => {
   const restaurant = useSelector(selectRestaurant);
 
   return (
-    <View className="bg-[#004AAD] flex-1">
+    <View className="bg-white flex-1">
       <SafeAreaView className="z-50">
         <View className="flex-row justify-between items-center p-5">
+          <Text className="font-light text-lg">Aide sur la commande</Text>
           <TouchableOpacity onPress={() => navigation.navigate("Home")}>
-            <XMarkIcon color="white" size={30} />
+            <XMarkIcon color="#004AAD" size={30} />
           </TouchableOpacity>
-          <Text className="font-light text-white text-lg">Aide sur la commande</Text>
         </View>
 
-        <View className="bg-[#FFDE59] mx-5 my-2 rounded-md p-6 z-50 shadow-lg">
+        <View className="bg-[#004AAD] mx-5 my-2 rounded-md p-6 z-50 shadow-lg">
+
           <View className="flex-row justify-between">
             <View>
-              <Text className="text-lg text-[#004AAD]">Arrivée de la commande</Text>
-              <Text className="text-4xl font-bold text-[#004AAD]">30-45 minutes</Text>
+              <Text className="text-lg text-white">Arrivée de la commande</Text>
+              <Text className="text-4xl font-bold text-[#FFDE59]">30-45 minutes</Text>
             </View>
+
             <Image
               source={require("../assets/scooter.gif")}
-              className="h-16 w-16"
+              className="h-20 w-16"
             />
           </View>
+
+          <Progress.Bar size={30} color="lightgray" indeterminate={true}/>
+
+          <Text className="mt-3 text-white italic">
+            Votre commande chez {restaurant.title} est en cours de préparation.
+          </Text>
+
         </View>
       </SafeAreaView>
+
+        <MapView  
+          initialRegion={{
+            latitude: restaurant.lat,
+            longitude: restaurant.long,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
+          }}
+          className="flex-1 -mt-10 z-0"
+          mapType="mutedStandard"
+        >
+          <Marker
+            coordinate={{
+              latitude: restaurant.lat,
+              longitude: restaurant.long,
+            }}
+            title={restaurant.title}
+            description={restaurant.short_description}
+            identifier="origin"
+            pinColor="00CCBB"
+          />
+        </MapView>
+        
+        <SafeAreaView className="bg-[#004AAD] flex-row items-center">
+          <Image
+            source={require("../assets/deliveryGuy.jpg")}
+            className="h-12 w-12 rounded-full p-4 ml-5"
+          />
+        </SafeAreaView>
+
+
     </View>
   );
 };
